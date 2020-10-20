554e8e011aabe3609a2ab09b45e9ef7b50aae7be
Ross Buddie <r.buddie@gmail.com>
2020-10-21 00:16:58 +0100

    Added percentage and Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_194908) do
+ActiveRecord::Schema.define(version: 2020_10_20_230639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_194908) do
     t.boolean "alcoholic"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "percentage"
   end
 
 end
